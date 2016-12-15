0548c86645098700d7a2cec104ed1f97eef6ea38
czzou <czzou@tencent.com>
2016-12-15 15:31:25 +0800

    push

---
+++ pm2/diffrentWriteStream.js
@@ -0,0 +1,11 @@
+/**
+ * Created by czzou on 2016/12/15.
+ */
+"use strict"
+
+const fs=require("fs");
+const wirteStream=fs.createWriteStream(`${process.pid}_log.txt`);
+let index=0;
+setInterval(function(){
+    wirteStream.write(`${new Date} log str ${index++} from process ${process.pid} \r\n`);
+},1000);
