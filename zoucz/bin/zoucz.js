05cd0b1b427481dd4b5303d0d606e5e93a38bd34
czzou <czzou@tencent.com>
2017-01-03 20:05:39 +0800

    push

--- zoucz/bin/zoucz.js
+++ zoucz/bin/zoucz.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
+
 const program = require('commander');
 const package = require('../package.json');
-
 program
     .version(`zoucz_v${package.version}`)
     .command('jiami [filename]', '加/解密名称为filename的文件,不填写则加/解密当前目录所有文件')
     .parse(process.argv);
-
-
