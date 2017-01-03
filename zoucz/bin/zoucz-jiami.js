8d59a9361d6ca95e07a7ea430e6858fb78ed0646
czzou <czzou@tencent.com>
2017-01-03 20:47:55 +0800

    push

--- zoucz/bin/zoucz-jiami.js
+++ zoucz/bin/zoucz-jiami.js
@@ -20,11 +20,13 @@ program.out=program.out || (program.jiemi?'__recover_output':'__jiami_output');
 
 let password=program.password;
 let outputPath=program.out;
+let promiseInfoArr=[];
+
 
 if(program.args.length>0){
     program.args.forEach((fileName)=>{
         try{
-            Jiami('./',outputPath,fileName);
+            CreateJiamiTask('./',outputPath,fileName);
         }
         catch(e){
             console.log(`加密${fileName}出错：${e.message}`);
@@ -32,42 +34,73 @@ if(program.args.length>0){
     });
 }
 else{
-    readAndJiami('./',outputPath);
+    readAllAndJiami('./',outputPath);
+}
+doJiamiWork();
+
+function doJiamiWork(){
+    let promise=new Promise((resolve,reject)=>{
+        let tasks=promiseInfoArr.splice(0,10);
+        let taskPromiseArr=[];
+        if(tasks.length==0){
+            reject(new Error("done"));
+            return;
+        }
+        tasks.forEach(function(task){
+            let taskPromise=new Promise(function (resolve,reject){
+                let cipher=null;
+                if(program.jiemi){
+                    cipher = crypto.createDecipher('aes192', password);
+                }
+                else{
+                    cipher = crypto.createCipher('aes192', password);
+                }
+                let input = fs.createReadStream(task.fromPath);
+                let output= fs.createWriteStream(task.toPath);
+                input.pipe(cipher).pipe(output);
+                console.log('【processing】',task.fromPath,'      【saving it to】',task.toPath);
+                output.on('finish', () => {
+                    resolve();
+                });
+            });
+            taskPromiseArr.push(taskPromise);
+        });
+        Promise.all(taskPromiseArr).then(doJiamiWork).catch((e)=>{
+            if(e.message==="done"){
+                console.log("任务处理完毕！");
+            }
+            else{
+                console.log(e);
+            }
+        });
+
+    });
+    return promise;
 }
 
-
-function readAndJiami(inputPath,outputPath){
+function readAllAndJiami(inputPath,outputPath){
     fs.mkdirSync(outputPath);
     let files=fs.readdirSync(inputPath);
     files.forEach((fileName)=>{
         //输出目录的文件不可加密
         if(path.resolve(inputPath,fileName)===path.resolve(outputPath)) return;
-        let fileInfo=fs.statSync(fileName);
+        let fileInfo=fs.statSync(path.resolve(inputPath,fileName));
         if(fileInfo.isFile()){
-            Jiami(inputPath,outputPath,fileName);
+            CreateJiamiTask(inputPath,outputPath,fileName);
         }
         else{
             let childInputPath=path.resolve(inputPath,fileName);
             let childOutputPath=path.resolve(outputPath,fileName);
-            readAndJiami(childInputPath,childOutputPath);
+            readAllAndJiami(childInputPath,childOutputPath);
         }
     });
 }
 
-function Jiami(inputPath,outputPath,fileName){
+function CreateJiamiTask(inputPath,outputPath,fileName){
     let fromPath=path.resolve(inputPath,fileName);
     let toPath=path.resolve(outputPath,fileName);
-    //加密并存储
-    console.log('【processing】',fromPath,'      【saving it to】',toPath);
-    let cipher;
-    if(program.jiemi){
-        cipher = crypto.createDecipher('aes192', password);
-    }
-    else{
-        cipher = crypto.createCipher('aes192', password);
-    }
-
-    let input = fs.createReadStream(fromPath);
-    let output= fs.createWriteStream(toPath);
-    input.pipe(cipher).pipe(output);
+    promiseInfoArr.push({
+        fromPath:fromPath,
+        toPath:toPath
+    });
 }
